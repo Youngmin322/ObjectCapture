fcf90f2e7cd6c2b12aa195c11563c31d59990472
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-12-12 16:00:41 +0900

    feat: AppDataModel을 AppFeature로 변경 후 TCA로 변환

---
+++ ObjectCapture/Models/AppFeature.swift
@@ -0,0 +1,108 @@
+//
+//  AppFeature.swift
+//  ObjectCapture
+//
+//  Created by Youngmin Cho on 12/4/25.
+//
+
+import SwiftUI
+import RealityKit
+import ComposableArchitecture
+
+@Reducer
+struct AppFeature {
+    // MARK: State
+    @ObservableState
+    struct State: Equatable {
+        var captureMode: CaptureMode = .object
+        var currentOrbit: Orbit = .orbit1
+        var isObjectFlipped: Bool = false
+        var isObjectFlippable: Bool = true
+        var hasIndicatedObjectCannotBeFlipped: Bool = false
+        var hasIndicatedFlipObjectAnyway: Bool = false
+        var tutorialPlayedOnce: Bool = false
+        
+        // MARK: Nested Types
+        enum CaptureMode: Equatable {
+            case object
+            case area
+            
+            var displayName: String {
+                switch self {
+                case .object: return "Object"
+                case .area: return "Area"
+                }
+            }
+            
+            var nextMode: CaptureMode {
+                switch self {
+                case .object: return .area
+                case .area: return .object
+                }
+            }
+        }
+        
+        enum Orbit: Int, CaseIterable, Identifiable {
+            case orbit1 = 1
+            case orbit2 = 2
+            case orbit3 = 3
+            
+            var id: Int { rawValue }
+            
+            var displayName: String {
+                switch self {
+                case .orbit1: return "First Pass"
+                case .orbit2: return "Second Pass"
+                case .orbit3: return "Third Pass"
+                }
+            }
+            
+            func next() -> Orbit {
+                switch self {
+                case .orbit1: return .orbit2
+                case .orbit2: return .orbit3
+                case .orbit3: return .orbit3
+                }
+            }
+        }
+    }
+    
+    // MARK: Action
+    enum Action {
+        case toggleCaptureMode
+        case resetState
+        case setCurrentOrbit(State.Orbit)
+        case flipObject
+    }
+    
+    // MARK: Reducer
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .toggleCaptureMode:
+                // 캡처 모드 변경
+                state.captureMode = state.captureMode.nextMode
+                return .none
+                
+            case .resetState:
+                // 상태 초기화
+                state.currentOrbit = .orbit1
+                state.isObjectFlipped = false
+                state.hasIndicatedObjectCannotBeFlipped = false
+                state.hasIndicatedFlipObjectAnyway = false
+                state.tutorialPlayedOnce = false
+                return .none
+                
+            case .setCurrentOrbit(let orbit):
+                // 현재 orbit 변경
+                state.currentOrbit = orbit
+                return .none
+                
+            case .flipObject:
+                // 객체 뒤집기
+                state.isObjectFlipped.toggle()
+                return .none
+            }
+        }
+    }
+}
