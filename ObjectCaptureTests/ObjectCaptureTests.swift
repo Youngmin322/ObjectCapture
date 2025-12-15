a42f2432fe697ce3a52529b99721812dc5c6245d
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-12-15 16:41:51 +0900

    feat: Testing을 위한 Unit Testing Bundle 추가

---
+++ ObjectCaptureTests/ObjectCaptureTests.swift
@@ -0,0 +1,16 @@
+//
+//  ObjectCaptureTests.swift
+//  ObjectCaptureTests
+//
+//  Created by Youngmin Cho on 12/15/25.
+//
+
+import Testing
+
+struct ObjectCaptureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
