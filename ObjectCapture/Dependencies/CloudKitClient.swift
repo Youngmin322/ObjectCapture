c83c97887f09235bc1bd25dea7bdb65f74c97e0a
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-12-21 16:11:28 +0900

    feat: 로컬 서버에 업로드를 위한 기능 구축

---
+++ ObjectCapture/Dependencies/CloudKitClient.swift
@@ -0,0 +1,8 @@
+//
+//  CloudKitClient.swift
+//  ObjectCapture
+//
+//  Created by Youngmin Cho on 12/17/25.
+//
+
+import Foundation
---
+++ ObjectCapture/Dependencies/NetworkClient.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkClient.swift
+//  ObjectCapture
+//
+//  Created by Youngmin Cho on 12/21/25.
+//
+
+import Foundation
